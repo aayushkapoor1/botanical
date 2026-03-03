cdf4375a95eccd651e122d984bf36c20437fcfe0
JeffersonFu09 <fujefferson21@gmail.com>
2026-03-03 15:38:18 -0500

    update grid size on sweep

--- cv_work/scan_water.py
+++ cv_work/scan_water.py
@@ -33,8 +33,8 @@ SERIAL_PORT = None
 
 # --- Scan grid ---
 # The raster scan will visit ROWS x COLS "cells".
-COLS = 7
-ROWS = 7
+COLS = 9
+ROWS = 3
 
 # Step size between cells in mm (the ESP converts mm to steps)
 STEP_X_MM = 50.0
